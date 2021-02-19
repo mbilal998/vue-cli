8192e7b2c769f0080970575835570949e379541f
MuhammadBilal <m.bilal998@gmail.com>
2021-02-19 12:25:45 +0500

    initial Commit with template Ref

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div>
+    <input type="text" ref="name" />
+    <button @click="handleClick()">Click</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue"
+
+export default {
+  data() {
+    return {
+      title: "My First Vue App :)",
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add("active")
+      this.$refs.name.focus()
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
