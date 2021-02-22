e9b7dd2b084220cab648096bf301381a18f33e36
MuhammadBilal <m.bilal998@gmail.com>
2021-02-22 18:46:25 +0500

    Before Moving into form

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,28 @@
     <input type="text" ref="name" />
     <button @click="handleClick()">Click</button>
   </div>
-  <Modal />
+  <p>welcome....</p>
+
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="" @click="toggleModal">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+      <template v-slot:links>
+        <a href="">Sign Up</a>
+        <a href="">More Info</a>
+      </template>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @click="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>Grab your ninja swag for half price</p>
+    </Modal>
+  </teleport>
+
+  <button @click.alt="toggleModal">Open Modal (Option)</button>
+  <button @click.alt="toggleModalTwo">Open Modal (Option) 2</button>
 </template>
 
 <script>
@@ -18,20 +39,30 @@ export default {
   data() {
     return {
       title: "My First Vue App :)",
+      header: "Google Calendar",
+      text: "I tried every thing but fail what I want to achieve",
+      showModal: false,
+      showModalTwo: false,
     }
   },
   methods: {
     handleClick() {
-      console.log(this.$refs.name)
       this.$refs.name.classList.add("active")
       this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     },
   },
 }
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
-      <h1>Modal Title</h1>
-      <p>Modal Content</p>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close")
+    },
+  },
+}
+</script>
+
 <style scoped>
 .modal {
   width: 400px;
@@ -21,9 +35,41 @@
   width: 100%;
   height: 100;
 }
-h1 {
+.modal h1 {
   color: #03cfb4;
   border: none;
   padding: 0;
+}
+.modal p {
+  font-style: normal;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h1 {
+  color: white;
+}
+
+.modal.sale .actions {
+  color: white;
+}
+.modal.sale .actions a {
+  color: white;
 }
 </style>
